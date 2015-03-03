197485e64c4dbc9973be63a3de801e7684306039
Byrdann Fox <mobileupstate@gmail.com>
2015-03-03 03:56:54 -0500

    Updates...

---
+++ iOSCoreData/iOSCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSCoreData
+//
+//  Created by Byrdann Fox on 3/3/15.
+//  Copyright (c) 2015 lycan.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSCoreData/iOSCoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSCoreData
+//
+//  Created by Byrdann Fox on 3/3/15.
+//  Copyright (c) 2015 lycan.io. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
